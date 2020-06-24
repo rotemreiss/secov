02e46ec21054a2e8afa091a64462392997260ac9
Rotem Reiss <reiss.r@gmail.com>
2020-06-25 01:45:12 +0300

    Support multiline annotations and provide fixes to more end cases

--- secov/main.py
+++ secov/main.py
@@ -30,6 +30,19 @@ def dir_path(path):
         raise argparse.ArgumentTypeError(f"readable_dir:{path} is not a valid path")
 
 
+def copytree(src, dst, symlinks=False, ignore=None):
+    """ Copy an entire directory.
+    Used for cloning our project directory before the parsing starts.
+    """
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            shutil.copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
+
+
 def get_code_file_names(path, extensions):
     code_files = []
     for root, dirs, files in os.walk(path):
@@ -45,16 +58,47 @@ def generate_route_uniqid(route):
     return uniqid.hexdigest()
 
 
+def clean_multiline_annotations(filepath, regex):
+    """ Support multiline annotations.
+    Multiline annotations must be cleaned for our parsing to work.
+    This is a recursive method that runs until there's nothing else to clean.
+    """
+    is_multiline_found = False
+
+    updated_code = ""
+    # Iterate over the lines and search for unclosed annotations.
+    with open(filepath, encoding="ISO-8859-1") as f:
+        for line in f:
+            if regex.search(line):
+                if not line.rstrip().endswith(")"):
+                    # Found unclosed annotation - flag it and strip the new line character/s.
+                    is_multiline_found = True
+                    line = line.rstrip()
+
+            updated_code += line
+
+    # Found multiline annotation - save the updated content to the source code and rerun the clean method.
+    if is_multiline_found:
+        f = open(filepath, "w")
+        f.write(updated_code)
+        f.close()
+
+        clean_multiline_annotations(filepath, regex)
+
+
+
 def grep_code_annotations(filepath, regex, base_path):
     res = []
     base_route = ""
+
+    clean_multiline_annotations(filepath, regex)
 
     base_path_len = len(base_path)
     with open(filepath, encoding="ISO-8859-1") as f:
         for line in f:
             if regex.search(line):
-                is_base_route = line.startswith("@Request")
-                clean_ann = line.lstrip(" ").rstrip("\n")
+                clean_ann = line.lstrip(" ").rstrip("\n").replace("  ", "")
+                is_base_route = clean_ann.startswith("@Request")
 
                 # Extract the route from the annotation's content
                 route = get_route_from_annotation(clean_ann, is_base_route)
@@ -70,7 +114,7 @@ def grep_code_annotations(filepath, regex, base_path):
                     # "route": get_route_from_annotation(clean_ann) if not is_base_route else "",
                     "route": route,
                     "is_base_route": is_base_route,
-                    "http_method": "" if is_base_route else re.search(r'@([a-zA-z]+)Mapping', line).group(1).upper()
+                    "http_method": get_method_from_annotation(clean_ann, is_base_route)
                 }
 
                 # Generate a uniqid for the route
@@ -107,8 +151,10 @@ def grep_test_annotations(filepath, regex):
     return res
 
 
-# @todo Refactor and remove the ugly type option
 def grep_annotations_multiple_files(files_list, regex, base_path, grep_type):
+    """
+    :todo Refactor and remove the ugly type option
+    """
     annotations_list = []
     for f in files_list:
         annotations = []
@@ -149,14 +195,31 @@ def get_first_route(route):
         return ""
 
 
+def get_method_from_annotation(annotation, is_base_route):
+    http_method = ""
+    if is_base_route:
+        content = get_route_annotation_content(annotation)
+        method_val = re.search(r'method=(.*?),', content)
+        http_method = method_val.group(1) if method_val else ""
+        # print(http_method)
+    else:
+        http_method = re.search(r'@([a-zA-z]+)Mapping', annotation).group(1).upper()
+
+    # Make sure that it matches one of the common HTTP methods.
+    method_val = re.search(r'GET|HEAD|POST|PUT|DELETE|CONNECT|OPTIONS|TRACE|PATCH', http_method)
+    http_method = method_val.group(0) if method_val else ""
+
+    return http_method
+
+
 def get_route_from_annotation(annotation, is_base_route):
     content = get_route_annotation_content(annotation)
 
     rel_placeholder = "[BASEPATH]" if not is_base_route else ""
 
     # Now do the heavy lifting - extract the path/s from the content
-    value_el = re.search(r'value=((.)+),', content)
-    path_el = re.search(r'path=((.)+),', content)
+    value_el = re.search(r'value=(.*?),', content)
+    path_el = re.search(r'path=(.*?),', content)
     if value_el:
         # Relative path - return with our placeholder
         return rel_placeholder + get_first_route(value_el.group(1))
@@ -168,7 +231,15 @@ def get_route_from_annotation(annotation, is_base_route):
         return rel_placeholder + get_first_route(content)
 
 
+def cleanup_tmp(tmp_project_dir):
+    # Cleanup - remove our temp project directory.
+    shutil.rmtree(tmp_project_dir, True)
+
+
 def main(db_path, project, git_repo, git_branch="master", project_dir=False, code_extensions="java,class", test_extensions="groovy"):
+
+    # Print our banner
+    banner()
 
     # Clone repository in case of a git repository instead of a local URI.
     if git_repo:
@@ -183,24 +254,25 @@ def main(db_path, project, git_repo, git_branch="master", project_dir=False, cod
         shutil.rmtree(project_dir, True)
         Repo.clone_from(git_repo, project_dir, branch=git_branch)
 
-    # Print our banner
-    banner()
+    tmp_project_dir = f'{project_dir}_tmp'
+    cleanup_tmp(tmp_project_dir)
+    copytree(project_dir, tmp_project_dir)
 
     #@todo Extend to support @RequestHeader and @RequestParam
 
     code_ext = code_extensions.split(",") if "," in code_extensions else code_extensions
-    code_files = get_code_file_names(project_dir, tuple(code_ext))
+    code_files = get_code_file_names(tmp_project_dir, tuple(code_ext))
 
     # @todo Split to a mappings file (yaml/json/..)
     java_route_regex = re.compile(r'@(.)+Mapping')
 
-    routes = grep_annotations_multiple_files(code_files, java_route_regex, project_dir, 'code')
+    routes = grep_annotations_multiple_files(code_files, java_route_regex, tmp_project_dir, 'code')
 
     # Get the tests coverage annotations
     test_annotation_regex = re.compile(r'@CoveredRoute')
     test_ext = test_extensions.split(",") if "," in test_extensions else test_extensions
-    test_files = get_code_file_names(project_dir, tuple(test_ext))
-    test_annotations = grep_annotations_multiple_files(test_files, test_annotation_regex, project_dir, 'tests')
+    test_files = get_code_file_names(tmp_project_dir, tuple(test_ext))
+    test_annotations = grep_annotations_multiple_files(test_files, test_annotation_regex, tmp_project_dir, 'tests')
 
     # Init DB.
     db.db_install(db_path)
@@ -215,6 +287,8 @@ def main(db_path, project, git_repo, git_branch="master", project_dir=False, cod
     db.insert_tests(test_annotations, pid)
 
     db.close()
+
+    cleanup_tmp(tmp_project_dir)
 
     # Print the results
     print("[+] Found and stored {} routes.".format(str(len(routes))))
